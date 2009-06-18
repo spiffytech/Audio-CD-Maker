9be3573064af07202502becd7677162367034476
Brian Cottingham <brian@linux-xyhc.site>
2009-06-18 09:49:11 -0400

    Did a little refactoring to clean up the code

--- audio_cd_creator.py
+++ audio_cd_creator.py
@@ -7,7 +7,12 @@
 import os
 import sys
 
-import mad
+try:
+    import mad
+except:
+    print "You must have the pymad library to use this script!"
+    exit()
+
 
 CD_LENGTH = 80 * 60 * 1000
 destdir = "dest"
@@ -21,46 +26,39 @@ def main():
         print "You must specify a directory containing your music!"
         exit()
 
-    retrieveFileLengths(sys.argv[1])
+    # Find all mp3 files and store their track lengths
+    for f in os.popen("find %s -iname '*.mp3'" % sys.argv[1]).read().split("\n"):
+        if f == '':
+            break
+        length = int(mad.MadFile(f).total_time())
+        files.append((f, length))
+
     if len(files) == 0:
         print "No files to handle!"
         exit()
 
-    cds.append([])  # Create first CD
-
     # Find a CD for each track to belong to 
+    cds.append([])  # Without this, the inner loop will never trigger, so we'll never have cds appended to the list, and the script will do nothing
     for track in files:
-        couldInsert = False
         for cd in range(len(cds)):  # Check every CD to see if we have room for this track
             if track[1] + timeOnCD(cd) < CD_LENGTH:
                 # Add the track to the current CD and move on to the next track
                 cds[cd].append(track)
-                couldInsert = True
                 break
-        if couldInsert == False:
-            cds.append([])
-            cds[len(cds)-1].append(track)
+            if cd == len(cds)-1:  # Searched all CDs but couldn't find a place to insert this track
+                cds.append([])
+                cds[len(cds)-1].append(track)
 
-    # Now that we've organized everything internally, organize the actual files
+    # Now that we've figured out how to organize everything, organize the actual files
     if not os.path.exists(destdir):
         os.mkdir(destdir)
     for cd in range(len(cds)):
         destination = os.path.join(destdir, str(cd))
         if not os.path.exists(destination):
             os.mkdir(destination)
+
         for track in cds[cd]:
             os.popen("cp %s %s" % (sh_escape(track[0]), sh_escape(destination)))  # Really, Python doesn't have a decent cp command built into the os module?
-
-
-
-def retrieveFileLengths(filedir):
-    '''Scans all of the mp3 files in the specified folder and stores their track lengths'''
-    for f in os.popen("find %s -name '*.mp3'" % filedir).read().split("\n"):
-#        f = os.path.join(filedir, f)
-        if f[-3:].lower() != "mp3":
-            continue
-        length = int(mad.MadFile(f).total_time())
-        files.append((f, length))
 
 
 
